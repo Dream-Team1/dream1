b0994961f26c0b314757b12de8ff62c1701e03c6
JessicaShalomG <jsphxusa072@gmail.com>
2018-10-03 09:47:05 -0500

    deletebutton

--- react-client/src/planer.jsx
+++ react-client/src/planer.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import Maps from "./planer/map.js";
+import TodoList from "./planer/TodoList.js";
 
 class Planer extends React.Component {
+
   render () {
     return (
         <div>
           <h1>this is the planner</h1>
+          <Maps />
+          <TodoList />
         </div>
 
     )
