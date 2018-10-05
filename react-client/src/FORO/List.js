951c17cfa527781f97bc429fd24d97b798a767fd
JessicaShalomG <jsphxusa072@gmail.com>
2018-10-05 13:20:19 -0500

    fixed forun working

--- react-client/src/FORO/List.js
+++ react-client/src/FORO/List.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import Comments from './Comments.js';
+import Post1 from './Post.js';
 
 
 const List = (props) => (
-
-
     <div className="posts">
- {props.posts.map((post) =>  <Comments post={post} key={post.id}/>)}
+ {props.posts.map((post) =>  <Post1 post={post} key={post.id}/>)}
  </div>
-
-
 );
 
 export default List;
