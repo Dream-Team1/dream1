2f9b043897c889c1729a7e1b761ea36cc062a58d
JessicaShalomG <jsphxusa072@gmail.com>
2018-10-08 20:12:38 -0500

    save change

--- react-client/src/home.jsx
+++ react-client/src/home.jsx
@@ -3,7 +3,7 @@ import FooterPage from "./components/footer.js";
 import Stars from './components/starRating.js';
 import Forum from "./components/Forum.js";
 import Main from "./components/search.js";
-import Uploader from "./Uploader/Uploader.js";
+
 
 class Home extends React.Component {
   render () {
@@ -24,9 +24,9 @@ class Home extends React.Component {
           <br/>
           <Stars />
           <br/>
+          <Face />
           <br/>
           <FooterPage />
-
         </div>
 
     )
