592a46187926820f289d79d24c7d4b0d82f2417e
SthefaniaRodriguezL <stherod13@gmail.com>
2018-10-09 23:20:31 -0500

    update value in rating

--- react-client/src/components/starRating.js
+++ react-client/src/components/starRating.js
@@ -6,9 +6,8 @@ class Stars extends React.Component {
    constructor(props) {
      super();
      this.state = {
-
+        starRating: 0
      }
-
    }
    handleRating(val) {
      console.log("this is the rating change...", val)
@@ -29,7 +28,11 @@ class Stars extends React.Component {
       url: '/getRating',
       method: 'GET',
     success: (results) => {
-    console.log("this is the results in the get request", results)
+      console.log("this is the results in the get request", results)
+      //results[0]["key"]
+      //get the value inside the object
+      var res = results[0]["AVG(val)"];
+      this.setState({starRating:res});
     },
     error: (xhr, err) => {
       console.log('err', err);
@@ -37,15 +40,15 @@ class Stars extends React.Component {
     })
   }
 
-
   render() {
     return(
 
     //  <link href="https://use.fontawesome.com/release/v5.0.8/css/all.css" rel="stylesheet">
       // <form target= "_self" method="GET">
       <div>
-    
+
        <Rating onChange={this.handleRating.bind(this)} />
+       <div>{this.state.starRating}</div>
 
       </div>
       // </form>
