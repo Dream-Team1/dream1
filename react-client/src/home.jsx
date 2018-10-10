894ae4c9bdacf572e21350c0922df989061478cc
albertosanchez.hc@gmail.com <albertosanchez.hc@gmail.com>
2018-10-10 15:31:35 -0500

    fixed footer

---
+++ react-client/src/home.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import FooterPage from "./components/footer.js";
+import Stars from './components/starRating.js';
+import Forum from "./components/Forum.js";
+import Main from "./components/search.js";
+
+
+class Home extends React.Component {
+
+  render () {
+    return (
+        <div>
+          <h1>Main Home</h1>
+          <Forum />
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <Main />
+          <br/>
+          <br/>
+          <Stars />
+          <br/>
+          <br/>
+          <FooterPage />
+        </div>
+
+    )
+  }
+}
+export default Home;
