0d791190e49041cf4049d01847cf769445a2c139
JessicaShalomG <jsphxusa072@gmail.com>
2018-10-22 16:11:49 -0500

    homepage done

--- react-client/src/links.jsx
+++ react-client/src/links.jsx
@@ -15,33 +15,33 @@ import "../dist/link.css"
       <div className="row">
         <div className="col-md-3">
         <h3> Bautizo </h3>
-        <Link to="/bautizo"><img src="https://i.imgur.com/PICobee.jpg" alt="Bautizo party theme" /><br/></Link>
+        <Link to="/bautizo"><img className="link" src="https://i.imgur.com/PICobee.jpg" alt="Bautizo party theme" /><br/></Link>
         </div>
         <div className="col-md-3">
           <h3> Wedding </h3>
-          <Link to="/wedding"><img src="https://i.imgur.com/MzzBCIn.jpg" alt="Weding party theme"  /><br/></Link>
+          <Link to="/wedding"><img className="link" src="https://i.imgur.com/MzzBCIn.jpg" alt="Weding party theme"  /><br/></Link>
         </div>
         <div className="col-md-3">
         <h3>Quinceañera </h3>
-        <Link to="/quince"><img src="https://i.imgur.com/Pmx44D8.jpg" alt="Quinseañera party theme" /><br/></Link>
+        <Link to="/quince"><img className="link" src="https://i.imgur.com/Pmx44D8.jpg" alt="Quinseañera party theme" /><br/></Link>
         </div>
         <div className="col-md-3">
         <h3> Kids Party </h3>
-          <Link to="/kids"><img src="https://i.imgur.com/8zjnqVr.jpg" alt="Kids Party theme" /><br/></Link>
+          <Link to="/kids"><img className="link" src="https://i.imgur.com/8zjnqVr.jpg" alt="Kids Party theme" /><br/></Link>
         </div>
       </div>
       <div className="row">
         <div className="col-md-4">
         <h3> Formal Party </h3>
-        <Link to="/formal"><img src="https://i.imgur.com/MoCFtRl.jpg" alt="Formal Party theme" /><br/></Link>
+        <Link to="/formal"><img className="link" src="https://i.imgur.com/MoCFtRl.jpg" alt="Formal Party theme" /><br/></Link>
         </div>
         <div className="col-md-4">
         <h3 className="BabyShower"> Baby Shower </h3>
-        <Link to="/baby"><img src="https://i.imgur.com/Ya3IenP.jpg" alt="Baby Showertheme" /><br/></Link>
+        <Link to="/baby"><img className="link" src="https://i.imgur.com/Ya3IenP.jpg" alt="Baby Showertheme" /><br/></Link>
       </div>
         <div className="col-md-4">
         <h3> Random </h3>
-          <Link to="/random"><img src="https://i.imgur.com/SBWnBH9.jpg" alt="Random party theme" /><br/></Link>
+          <Link to="/random"><img className="link" src="https://i.imgur.com/SBWnBH9.jpg" alt="Random party theme" /><br/></Link>
         </div>
       </div>
 
