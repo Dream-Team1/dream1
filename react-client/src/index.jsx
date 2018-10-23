d000127205eb21d467286730867a00447b380ad8
albertosanchez.hc@gmail.com <albertosanchez.hc@gmail.com>
2018-10-23 00:22:50 -0500

    trying

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -17,27 +17,27 @@ import Login from "./LogIn.jsx";
 import LogOut from './LogOut.js';
 
 class App extends React.Component {
-  render () {
-    return (
-      <BrowserRouter>
-        <div>
-            <Switch>
-            <Route   path="/login" component={Main}/>
-              <Route exact path="/" component={Principal}/>
-              <Route path="/quince" component={Home}/>
 
-              <Route path="/wedding" component={HomeWedding}/>
-              <Route path="/bautizo" component={HomeBautizo}/>
-              <Route path="/kids" component={HomeKids}/>
-              <Route path="/baby" component={HomeBaby}/>
-              <Route path="/formal" component={HomeFormal}/>
-              <Route path="/random" component={HomeRandom}/>
+ render () {
+   return (
+     <BrowserRouter>
+           <Switch>
+             <Route   path="/login" component={Main}/>
+             <Route exact path="/" component={Principal}/>
+             <Route path="/quince" component={Home}/>
 
-            </Switch>
-          </div>
-      </BrowserRouter>
-    )
-  }
+             <Route path="/wedding" component={HomeWedding}/>
+             <Route path="/bautizo" component={HomeBautizo}/>
+             <Route path="/kids" component={HomeKids}/>
+             <Route path="/baby" component={HomeBaby}/>
+             <Route path="/formal" component={HomeFormal}/>
+             <Route path="/random" component={HomeRandom}/>
+
+           </Switch>
+     </BrowserRouter>
+
+   )
+ }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
